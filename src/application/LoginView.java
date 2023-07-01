9dd8ec5a24f48583cd3d56d7748a0ea8856f0ac6
Avinash Puppala <anpuppal@gmail.com>
2023-07-01 16:11:41 -0700

    Basic Labels and Input Fields for Login Page View. Still requires validation. Main Controller class will control switch once validation is run

--- src/application/LoginView.java
+++ src/application/LoginView.java
@@ -1,20 +1,47 @@
 package application;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class LoginView {
 
     private Scene scene;
 
     public LoginView() {
-        VBox layout = new VBox();
-        // TODO: add UI components to layout
+        GridPane gridPane = new GridPane();
 
-        scene = new Scene(layout, 300, 200);
+        // Padding and Spacing
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+
+        // Username label and text field
+        Label usernameLabel = new Label("Username:");
+        TextField usernameField = new TextField();
+        gridPane.add(usernameLabel, 0, 0);
+        gridPane.add(usernameField, 1, 0);
+
+        // Password label and password field
+        Label passwordLabel = new Label("Password:");
+        PasswordField passwordField = new PasswordField();
+        gridPane.add(passwordLabel, 0, 1);
+        gridPane.add(passwordField, 1, 1);
+
+        // Login button
+        Button loginButton = new Button("Login");
+        gridPane.add(loginButton, 1, 2);
+
+        // Setting up the scene
+        scene = new Scene(gridPane, 300, 200);
     }
 
     public Scene getScene() {
         return scene;
     }
 }
+
